a00673e266bafd48a7d771799f3407043b031cb0
Thieu Le Quang <quangthieuagu@gmail.com>
2017-04-29 13:31:20 +0700

    add root service for microservice

---
+++ src/Providers/RootServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace RMS\Providers;
+
+/**
+ * Class RootServiceProvider
+ * @package RMS\Providers
+ */
+class RootServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+    /**
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+}
